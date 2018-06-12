1443e24ef46f146cdc65841e468b5c3aa4729df9
jiyanpingtest1 <jiyanping01@163.com>
2018-06-12 16:33:09 +0800

    test commit

---
+++ src/com/kethy/hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.kethy.hello;
+
+public class HelloWorld {
+
+	public void sayHello(String str) {
+		System.out.println(str);
+	}
+}
---
+++ src/com/kethy/test/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package com.kethy.test;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void testSayHello() {
+		
+	}
+	
+}
